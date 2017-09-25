8cf682ed83904b0fca6dda052b5ca17eda23de17
Del-Rick <rickmaxg3/2hotmail.com>
2017-09-25 14:55:59 -0300

    test opsrb

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cookbooks
+# my_first_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: my_first_cookbook
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['packages-cookbook'] = [
+ 'nodejs',
+ 'imagemagick'
+]
